81efc69aa1d2d9aeff237b0cf5d3fa5c03f401f2
Kevin <kevinbruggiero@gmail.com>
2019-05-21 13:49:13 -0600

    done.

--- CleanApp/app/controllers/api/v1/users_controller.rb
+++ CleanApp/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
-class Api::V1::UsersController < ApplicationController
+require "pry"
 
+class Api::V1::UsersController < ApplicationController
+skip_before_action :verify_authenticity_token
 
   def index
     @users = User.all
@@ -8,8 +10,9 @@ class Api::V1::UsersController < ApplicationController
 
 
   def update
-      @user = User.find(params[:id])
-    @user.update(params.permit[:points])
+    @user = User.find(params[:id])
+    @user.points = params[:points]
+    @user.save
   end
 
 
